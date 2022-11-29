3522b44529422262625e3fdfb4b32394ca6b1240
IgnacioUoM <116823205+IgnacioUoM@users.noreply.github.com>
2022-11-29 18:04:08 +0000

    Add files via upload

---
+++ Valve_Class_Test.ino
@@ -0,0 +1,20 @@
+Valve myvalve(3);
+
+Servo myservo2;
+void setup(){
+  myvalve.setUp();
+
+}
+
+int a=0,b=80;
+void loop(){
+  myvalve.set_pos1();
+  delay(500);
+  delay(2000);
+  myvalve.set_pos0();
+  delay(500);
+  delay(2000);
+  a = a+10;
+  b = b-10;
+  myvalve.set_angle_lims(a,b);
+}
