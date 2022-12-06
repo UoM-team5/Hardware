bcbce3e2a815fbebe0a9b6c32dacae4df2190ab1
DefinitelyBalanced <72870238+DefinitelyBalanced@users.noreply.github.com>
2022-12-06 00:54:45 +0000

    Shutter Class

---
+++ Shutter/PlatformIO_Build/include/Shutter.h
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+
+enum state{
+    closed = 120,
+    open = 0,
+    open2 = 240,
+};
+
+class Shutter{
+        private: 
+        Servo ShutterServo;
+        int input_pin, output_pin;
+
+    public:
+        Shutter(int inputpin, int outputpin);
+        // Constructor
+        // @param inputpin: arduino pin connection for the analog feedback
+        // @param outputpin: arduino pin connection for control of the servo
+
+        void Initialise();
+        //Initialises object, to be run in arduino setup 
+
+        void goto_open (double desired);
+        // Open loop control of servo position
+        // @param desired: Desired angle of the shutter in degrees
+       
+
+        double get();
+        // Returns a double of angle in degrees
+       
+
+        void goto_controlled (double desired);
+        // Closed loop control of servo position
+        // Cpp file may need altering to tune PI controller
+        // @param desired: Desired angle of the shutter in degrees
+
+        void moveto(state pos, bool contol);
+        //Moves shutter to preset positions
+        // @param pos: Desired preset position, edit state above to include different positions
+        // @param control: Set 0 to use goto_open, Set 1 to use goto_controlled
+
+
+
+};
