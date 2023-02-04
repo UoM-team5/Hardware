012bd608716fc1ca3e72b0079a015f18021dd6c8
Joshua Bettles <60298391+Joshua-Bettles@users.noreply.github.com>
2023-02-04 11:53:54 +0000

    Add files via upload

---
+++ ARCaDIUS_Pump_Demo/Valve.h
@@ -0,0 +1,46 @@
+#ifndef Valve_H
+#define Valve_H
+
+#include <Arduino.h>
+#include "Servo.h"
+
+class Valve {
+  private:
+    int pos0 = 0;
+    int pos1 = 1;
+    bool a = false;
+    int angle_0 = 1700; // ServoTimer 2 uses 550= min degrees and 2500 = max degrees, test per servo
+    int angle_1 = 2100;
+    int pinSer;  // pin number (should be 2 vor the servo)
+    Servo myservo;
+
+  public:
+    // Constructor and initial settings
+    Valve(int pinServo): pinSer(pinServo) {};
+
+    // Setup
+    void setUp();
+
+    // Set valve position to 1
+    void set_pos1(void);
+
+    // Set valve position to 0
+    void set_pos0(void);
+
+    // Toggle valve position
+    void toggle_pos(void);
+
+    // Set the limit position of the servo motor (in degrees)
+    void set_angle_lims(int angle0, int angle1);
+
+    // Get measurement of the servo motor (in degrees)
+    int get_pos_analog(void);
+
+    // Get state of valve (0 or 1)
+    bool get_pos_digital(void);
+
+    // Get measurement of the limit set for the servo motor (in degrees)
+    int get_lim(int pos);
+};
+
+#endif
