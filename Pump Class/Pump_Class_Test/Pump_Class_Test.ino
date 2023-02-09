bf3f74e8af1c35ee22435bc7c3923ecbb2ee7b51
IgnacioUoM <ignacio.villanueva@student.manchester.ac.uk>
2023-02-09 17:01:34 +0000

    Update - Stepper calibration

---
+++ Pump Class/Pump_Class_Test/Pump_Class_Test.ino
@@ -0,0 +1,23 @@
+    /*#include <Servo.h>
+#import <Arduino.h>
+#include "Pump_Class.h"
+
+
+int flag=1;
+Pump ServoPump(4,0);
+Pump StepPump(5,2,8,1);
+
+void setup(){
+  Serial.begin(115200);
+  ServoPump.setUp();
+  StepPump.setUp();
+}
+
+void loop(){
+  if(flag==1){
+    StepPump.set_vol(2.5,1);
+    ServoPump.set_vol(2.5,1);
+    flag++;
+  }   
+}
+*/
