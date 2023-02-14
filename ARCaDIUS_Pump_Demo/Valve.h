1532dc711ec6c3fe7a090bb68ba2ce4f3d67aec2
IgnacioUoM <116823205+IgnacioUoM@users.noreply.github.com>
2023-02-14 17:16:03 +0000

    Update

--- ARCaDIUS_Pump_Demo/Valve.h
+++ ARCaDIUS_Pump_Demo/Valve.h
@@ -1,3 +1,5 @@
+#ifndef Valve_H
+#define Valve_H
 #include <Servo.h>
 
 class Valve{
@@ -7,8 +9,8 @@ class Valve{
     int posMid = 2;
     int a = 0;
     int angleMid = 90;
-    int angleOpen = 65;
-    int angleClose = 125;  
+    int angleOpen;
+    int angleClose;  
     int pinSer;  // pin number (should be 2 for the servo)
     Servo myservo;
 
@@ -16,44 +18,46 @@ class Valve{
     
   public:
 // Constructor and initial settings
-    Valve(int pinServo, int posOpen, int posClose):pinSer(pinServo), posOpen(posOpen), posClose(posClose){}
+    Valve(int pinServo, int posOpen, int posClose):pinSer(pinServo), angleOpen(posOpen), angleClose(posClose){}
 
 
 
 // Setup
-    void setUp(){};
+    void setUp();
 
 // Set Valve position to 0 (open), 1 (closed), or 2 (middle)
-    void set_pos(int pos){};
+    void set_pos(int pos);
 
 // Set valve position to 1
-    void set_pos1(void){};
+    void set_pos1(void);
 
 
 // Set valve position to 0
-    void set_pos0(void){};
+    void set_pos0(void);
 
 
 
 // Toggle valve position
-    void toggle_pos(void){};
+    void toggle_pos(void);
     
 
 
 // Set the limit position of the servo motor (in degrees)
-    void set_angle_lims(int angle0, int angle1){};
+    void set_angle_lims(int angle0, int angle1);
 
 
 
 // Get measurement of the servo motor (in degrees)
-    int get_pos_analog(void){};
+    int get_pos_analog(void);
 
 
 // Get state of valve (0 or 1)
-    int get_pos_digital(void){};
+    int get_pos_digital(void);
 
 
 
 // Get measurement of the limit set for the servo motor (in degrees)
-    int get_lim(int pos){};
+    int get_lim(int pos);
 };
+
+#endif
