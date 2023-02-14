62ed3dcd110fd1e65aa7afe3acc224baecde5478
IgnacioUoM <116823205+IgnacioUoM@users.noreply.github.com>
2023-02-14 17:15:19 +0000

    Update

--- ARCaDIUS_Pump_Demo/Valve.cpp
+++ ARCaDIUS_Pump_Demo/Valve.cpp
@@ -1,149 +1,62 @@
 #include <Servo.h>
 #include <Arduino.h>
-
-class Valve {
-  private:
-    int posOpen = 0;
-    int posClose = 1;
-    int posMid = 2;
-    int a = 0;
-    int angleMid = 90;
-    int angleOpen = 67;
-    int angleClose = 123;
-    int pinSer;  // pin number (should be 2 for the servo)
-    Servo myservo;
+#include "Valve.h"
 
 
+// Setup
+void Valve::setUp(void) {
+  myservo.attach(pinSer);
+  myservo.write(90);
+}
 
-  public:
-    // Constructor and initial settings
-    Valve(int pinServo, int AngO, int AngC): pinSer(pinServo), angleOpen(AngO), angleClose(AngC) {}
-
-
-
-    // Setup
-    void setUp() {
-      myservo.write(90);
-      myservo.attach(Valve::pinSer);
-    }
-
-
-    void set_pos(int pos) {
-      if (pos == 0) { // If pos is open
-        Valve::a = posOpen;
-        myservo.write(angleOpen);
-      }
-      if (pos == 1) { // If pos is closed
-        Valve::a = posClose;
-        myservo.write(angleClose);
-      }
-      if (pos == 2) { // If pos is middle
-        Valve::a = posMid;
-        myservo.write(angleMid);
-      }
-    }
-
-    /*
-
-      // Set valve position to 1
-        void set_pos1(void){
-          Valve::a = posClose;
-          myservo.write(angleClose);
-        } // End function
-
-
-      // Set valve position to 0
-        void set_pos0(void){
-          Valve::a = posClose;
-          myservo.write(angleClose);
-        } // End function
-
-
-      // Toggle valve position
-        void toggle_pos(void){
-          if (get_pos_digital() == 1){
-            set_pos0();
-          } // End if
-          else {
-            set_pos1();
-          } // End else
-        } // End funtion
-
-    */
-
-
-    // Set the limit position of the servo motor (in degrees)
-    void set_angle_lims(int angle0, int angle1) { // Angle0 is for open, Angle1 is for close
-      Valve::angleOpen = angle0;
-      Valve::angleClose = angle1;
-    } // End function
-
-
-
-    // Get measurement of the servo motor (in degrees)
-    int get_pos_analog(void) {
-      return myservo.read();
-    }
-
-
-
-    // Get state of valve (0 (open), 1 (closed), 2 (middle))
-    int get_pos_digital(void) {
-      if (a == 0) {
-        return 0;  // Open
-      } // End if
-      if (a == 1) {
-        return 1; // Closed
-      }// End if
-      if (a == 2) {
-        return 2; // Middle
-      }
-    } // End function
-
-
-
-    // Get measurement of the limit set for the servo motor (in degrees)
-    int get_lim(int pos) {
-      if (pos == 0) {
-        return Valve::angleOpen;
-      }
-      if (pos == 1) {
-        return Valve::angleClose;
-      }
-      if (pos == 3) {
-        return Valve::angleMid;
-      }
-    } // End function
-};
-
-/*
-  Valve myvalve(3,55,125);
-  Servo myservo2;
-
-  void setup(){
-  myvalve.setUp();
+void Valve::set_pos(int pos) {
+  a=pos;
+  if (pos == 0) { // If pos is open
+    myservo.write(angleOpen);
   }
-
-  int a=0,b=80;
-  void loop(){
-  myvalve.set_pos(0);
-  delay(500);
-  delay(1000);
-   myvalve.set_pos(2);
-  delay(500);
-  delay(1000);
-  myvalve.set_pos(1);
-  delay(500);
-  delay(1000);
-   myvalve.set_pos(2);
-  delay(500);
-  delay(1000);
-
-  a = a+10;
-  b = b-10;
-  myvalve.set_angle_lims(a,b);
-  myvalve.set_pos(0);
-  delay(500);
-  delay(2000);
+  else if (pos == 1) { // If pos is closed
+    myservo.write(angleClose);
   }
-*/
+  else if (pos == 2) { // If pos is middle
+    myservo.write(angleMid);
+  }
+  
+}
+
+
+// Set the limit position of the servo motor (in degrees)
+void Valve::set_angle_lims(int angle0, int angle1) { // Angle0 is for open, Angle1 is for close
+  angleOpen = angle0;
+  angleClose = angle1;
+} // End function
+
+
+
+// Get measurement of the servo motor (in degrees)
+int Valve::get_pos_analog(void) {
+  return myservo.read();
+}
+
+
+
+// Get state of valve (0 (open), 1 (closed), 2 (middle))
+int Valve::get_pos_digital(void) {
+  return a;
+  
+} // End function
+
+
+
+// Get measurement of the limit set for the servo motor (in degrees)
+int Valve::get_lim(int pos) {
+  if (pos == 0) {
+    return angleOpen;
+  }
+  if (pos == 1) {
+    return angleClose;
+  }
+  if (pos == 2) {
+    return angleMid;
+  }
+} // End function
+
