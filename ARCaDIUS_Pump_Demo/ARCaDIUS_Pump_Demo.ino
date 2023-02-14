be2e7b182dd1c80c92e465b6d2bc6f05e0a4d1c9
IgnacioUoM <116823205+IgnacioUoM@users.noreply.github.com>
2023-02-14 17:17:39 +0000

    Update

--- ARCaDIUS_Pump_Demo/ARCaDIUS_Pump_Demo.ino
+++ ARCaDIUS_Pump_Demo/ARCaDIUS_Pump_Demo.ino
@@ -1,5 +1,5 @@
 #include "ARCaDIUS_Serial.h"
-#include "Valve.h"
+#include "Valve_Class.h"
 #include "Pump.h"
 
 String DeviceDesc = "This is an example string";
@@ -14,19 +14,21 @@ int Num_of_Bubble = 5;
 int ResetPin = 3;
 
 ASerial Device(DeviceDesc, Device_ID, Sender_ID, Num_of_Pumps, Num_of_Valves, Num_of_Shutter, Num_of_Temp, Num_of_Bubble, Num_of_Mixer, ResetPin);
-Valve Valve1(9);
-Pump P1(5);
+Pump P1(9);
+Valve myvalve(5, 65, 120);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.setTimeout(1000);
+  Serial.println("start");
   Device.Start();
-  Valve1.setUp();
+  myvalve.setUp();
   P1.setUp();
 }
 
 void loop() {
+
   // Enter the subsystem code in the switch statement, delete all the serial prints.
   if (Device.GotCommand()) {
     switch (Device.GetCommand()) {
@@ -50,15 +52,13 @@ void loop() {
       case VALVE: // Enter code for Valve here
         Serial.println("The valve number is: " + (String)Device.getValve());
         switch (Device.getValve()) {
+          // [sID1000 rID1001 PK2 V1 S0]
           case 1:
-            if (Device.getValveState()) {
-              //Valve1.set_pos1();
-              Serial.println("The valve state  is: " + (String)Valve1.get_pos_digital());
-            }
-            else {
-              //Valve1.set_pos0();
-              Serial.println("The valve state  is: " + (String)Valve1.get_pos_digital());
-            }
+            myvalve.set_pos(Device.getValveState());
+            Serial.println("1The valve state  is: " + (String)Device.getValveState());
+            Serial.println("2The valve state  is: " + (String)myvalve.get_pos_digital());
+            Serial.println("3The valve angle  is: " + (String)myvalve.get_pos_analog());
+            
             break;
           default:
             break;
@@ -74,7 +74,7 @@ void loop() {
     delay(100);
     Device.FinishedCommand();
 
-    // This is how you update sensor values 1 to N, TEMP or BUBBLE 
-    Device.updateSensors(TEMP, 1, 123.456);  
+    // This is how you update sensor values 1 to N, TEMP or BUBBLE
+    Device.updateSensors(TEMP, 1, 123.456);
   }
 }
