806b328bed532ef2cebcfa413686707af827a204
DefinitelyBalanced <72870238+DefinitelyBalanced@users.noreply.github.com>
2023-02-27 18:07:34 +0000

    Created module folders

---
+++ New folder/Shutter module/ARCaDIUS_Pump_Demo.ino
@@ -0,0 +1,101 @@
+#include "ARCaDIUS_Serial.h"
+#include "Valve.h"
+#include "Pump.h"
+#include "Shutter.h"
+
+String DeviceDesc = "This is an example string";
+int Device_ID = 1001;
+int Sender_ID = 1000;
+int Num_of_Pumps = 2;
+int Num_of_Valves = 2;
+int Num_of_Shutter = 2;
+int Num_of_Mixer = 1;
+int Num_of_Temp = 4;
+int Num_of_Bubble = 5;
+int ResetPin = 3;
+
+ASerial Device(DeviceDesc, Device_ID, Sender_ID, Num_of_Pumps, Num_of_Valves, Num_of_Shutter, Num_of_Temp, Num_of_Bubble, Num_of_Mixer, ResetPin);
+Pump P1(9);
+Valve myvalve(5, 65, 120);
+Shutter shutter(54, 7);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.setTimeout(1000);
+  Serial.println("start");
+  Device.Start();
+  myvalve.setUp();
+  P1.setUp();
+  shutter.Initialise();
+}
+
+
+
+void loop() {
+
+  // Enter the subsystem code in the switch statement, delete all the serial prints.
+  if (Device.GotCommand()) {
+    switch (Device.GetCommand()) {
+      case PUMP: // Enter code for pumping here
+        Serial.println("The pump number is: " + (String)Device.getPump());
+        Serial.println("The pump volume in mls is: " + (String)Device.getPumpMls());
+        Serial.println("The pump direction is: " + (String)Device.getPumpDir());
+        switch (Device.getPump()) {
+          case 1:
+            //P1.set_vol(Device.getPumpMls(),Device.getPumpDir());
+            break;
+          default:
+            break;
+        }
+        break;
+      case MIXER: // Enter code for mixer here
+        Serial.println("The mixer number is: " + (String)Device.getMixer());
+        Serial.println("The mixer speed is: " + (String)Device.getMixerSpeed());
+        Serial.println("The mixer direction is: " + (String)Device.getMixerDir());
+        break;
+      case VALVE: // Enter code for Valve here
+        Serial.println("The valve number is: " + (String)Device.getValve());
+        switch (Device.getValve()) {
+          // [sID1000 rID1001 PK2 V1 S0]
+          case 1:
+            myvalve.set_pos(Device.getValveState());
+            Serial.println("1The valve state  is: " + (String)Device.getValveState());
+            Serial.println("2The valve state  is: " + (String)myvalve.get_pos_digital());
+            Serial.println("3The valve angle  is: " + (String)myvalve.get_pos_analog());
+            
+            break;
+          default:
+            break;
+        }
+        break;
+
+      case SHUTTER:  
+        Serial.println("The shutter number is: " + (String)Device.getShutter());
+        switch (Device.getShutterPos()) {
+          case 0:
+            //Serial.println("The shutter position  is: " + (String)Device.getShutterPos());
+            shutter.moveto(0, 0); //change 0 to 1 for control
+            break;
+          case 1:
+            //Serial.println("The shutter position  is: " + (String)Device.getShutterPos());
+            shutter.moveto(120, 0); //change 0 to 1 for control
+            break;
+          case 2:
+            //Serial.println("The shutter position  is: " + (String)Device.getShutterPos());
+            shutter.moveto(240, 0); //change 0 to 1 for control
+            break;
+          default:
+            break;
+        }
+
+      default: // Leave this, its just the default
+        break;
+    }
+    delay(100);
+    Device.FinishedCommand();
+
+    // This is how you update sensor values 1 to N, TEMP or BUBBLE
+    Device.updateSensors(TEMP, 1, 123.456);
+  }
+}
---
+++ New folder/Shutter module/Shutter.cpp
@@ -0,0 +1,58 @@
+#include <Arduino.h>
+#include <Servo.h>
+#include "Shutter.h"
+
+
+
+
+    Shutter::Shutter(int inputpin, int outputpin) :input_pin(inputpin), output_pin(outputpin) {};
+
+    void Shutter::Initialise() {
+        ShutterServo.attach(output_pin);
+        ShutterServo.write(90);
+        }
+
+    void Shutter::goto_open (double desired){
+        Serial.println("goto_open");
+        double Input = desired+26;
+        //Serial.println((unsigned int)((0.728*Input)-19.105));
+        ShutterServo.write((unsigned int)((0.728*Input)-19.105));
+        delay(500);
+    };
+
+    double Shutter::get(void){
+        double in = 0;
+        for(int i=0; i<10; i++){
+            in += analogRead(input_pin);
+            delay(5);
+        }
+        double Average = in/10;
+    return ((0.4826*Average-26));
+    };
+
+    void Shutter::goto_controlled (double desired){
+        uint16_t pos = ShutterServo.read();
+        double diff=((desired)-(this->get()));
+        //Serial.println(diff);
+        double Ki = 0;
+        while ((abs(diff))>3){
+            // Serial.println(diff);
+            double changepos= 0.3*diff;  //Kp
+            // Ki += 0.0004*diff;            //Ki
+            pos += (int) (changepos + Ki);
+            if (pos>180) pos = 180;
+            if (pos<0) pos =0;
+            ShutterServo.write(pos);
+            delay(150);
+            diff=((desired)-(this->get()));
+            Serial.println(diff);
+        }
+    };
+
+    void Shutter::moveto(state pos, bool controller){
+        if(controller) {
+            this->goto_controlled(pos);
+        }else{
+            this->goto_open(pos); 
+        };
+    };
