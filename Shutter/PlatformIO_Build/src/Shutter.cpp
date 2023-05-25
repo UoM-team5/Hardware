beab46d8ab1feaca4ecb4929095c84f3dc0dd83f
DefinitelyBalanced <72870238+DefinitelyBalanced@users.noreply.github.com>
2023-05-25 13:56:21 +0100

    testing code

--- Shutter/PlatformIO_Build/src/Shutter.cpp
+++ Shutter/PlatformIO_Build/src/Shutter.cpp
@@ -13,31 +13,49 @@
         }
 
     void Shutter::goto_open (double desired){
-        Serial.println("goto_open");
+        //Serial.println("goto_open");
         double Input = desired+26;
-        Serial.println((unsigned int)((0.728*Input)-19.105));
+        //Serial.println((unsigned int)((0.728*Input)-19.105));
         ShutterServo.write((unsigned int)((0.728*Input)-19.105));
         delay(500);
+
     };
 
     double Shutter::get(void){
-    return ((0.4826*analogRead(input_pin)-26));
+        float average;
+        //Serial.println("ave:");
+        average = analogRead(input_pin);
+        //Serial.println(average);
+        for(int i = 0; i<3; i++){
+            
+            average += analogRead(input_pin);
+            //Serial.println(average);
+            delay(5);
+        }
+        average = average/4.0;
+        //Serial.println("Reading:");
+        //Serial.println(average);
+        //Serial.println(analogRead(input_pin));
+        
+    return (0.4826*average);
     };
 
     void Shutter::goto_controlled (double desired){
-        uint16_t pos = ShutterServo.read();
+        float pos = ShutterServo.read();
         double diff=((desired+26)-(0.4826*analogRead(input_pin)));
         double Ki = 0;
-        while ((abs(diff))>3){
-            // Serial.println(diff);
-            double changepos= 0.1*diff;  //Kp
-            Ki += 0.001*diff;            //Ki
+        double dt = 100;
+        while ((abs(diff))>3.0){
+            //Serial.println(diff);
+            double changepos= 0.08*diff;  //Kp
+            Ki += 0.4*diff*(dt/1000);            //Ki
             pos += (int) changepos + (int) Ki;
             if (pos>180) pos = 180;
             if (pos<0) pos =0;
-            ShutterServo.write(pos);
-            delay(100);
-            diff=((desired+26)-(0.4826*analogRead(input_pin)));
+            ShutterServo.write((uint16_t)pos);
+            delay(dt);
+            //Serial.println(pos);
+            diff=((desired+26)-(this->get()));
         }
     };
 
--- Shutter/PlatformIO_Build/src/main.cpp
+++ Shutter/PlatformIO_Build/src/main.cpp
@@ -1,23 +1,40 @@
 #include <Arduino.h>
 #include <Servo.h>
 #define Analog_pin 14
-#define Servo_pin 6
+#define Servo_pin 5
 #include "Shutter.h"
 
 Shutter shutter(Analog_pin, Servo_pin);
 
 void setup() {
   Serial.begin(9600);
-  delay(5000);
+  delay(1000);
   shutter.Initialise();
+  delay(5000);
 }
 
-void loop() {
+int start;
+int time;
+int iterations= 0;
 
-  // shutter.moveto(open, 0);
-  // delay(2000);
-  // shutter.moveto(closed, 0);
-  // delay(2000);
+void loop() {
+  iterations++;
+  Serial.println(iterations);
+  Serial.println("Opening");
+  start = millis();
+  shutter.moveto(open, 1);
+  time = millis() - start;
+  Serial.println(time);
+  Serial.println(shutter.get());
+  delay(2000);
+
+  Serial.println("Closing");
+  start = millis();
+  shutter.moveto(closed, 1);
+  time = millis() - start;
+  delay(2000);
+  Serial.println(time);
+  Serial.println(shutter.get());
   // shutter.moveto(open2, 0);
   // delay(2000);
   // shutter.moveto(closed, 0);
