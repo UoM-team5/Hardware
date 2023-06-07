3dbeee11458daaf18c183df0c33ae940ce4c12e8
DefinitelyBalanced <72870238+DefinitelyBalanced@users.noreply.github.com>
2023-06-07 11:23:06 +0100

    Finished

--- Classes/Shutter/Shutter.h
+++ Classes/Shutter/Shutter.h
@@ -43,9 +43,9 @@ class Shutter{
         // @param control: Set 0 to use goto_open, Set 1 to use goto_controlled
 
         void timedExposure(state pos, unsigned int exposure_time, bool controller);
-        // Moves shutter to preset position then returns to original position 
+        // Moves shutter to preset position then returns to original position after a set time has elapsed 
         // @param pos: Desired preset position, edit state above to include different positions
-        // @para  exposure_time: Miliseconds  
+        // @para  exposure_time: Miliseconds  before returning to original position
         // @param controller: Set 0 to use goto_open, Set 1 to use goto_controlled
 
 };
---
+++ Tests/Radiation Testing/README.md
@@ -0,0 +1,2 @@
+This was the python code used to conduct the gamma irradiator testing using a custom datalogger supplied by a team member.
+Details of this datalogger have not been included (as actual documentation doesn't exist), however the code used to run the experiment has been included to show how the data was recorded.
